7b27f326fb7a4d823188d9a81eb3b1e72d90dc10
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-03-24 10:19:14 -0700

    remove word splitting

--- scripts/config.js
+++ scripts/config.js
@@ -1,130 +1,170 @@
 let defaultGameConfig = {
+  libs: {
+    wordFindScript: 'assets/libs/wordfind.js',
+  },
 
-    libs:{
-        wordFindScript:'assets/libs/wordfind.js'        
-    },
+  images: {
+    transp: 'assets/images/transp.png',
+    preloaderLoading: 'assets/images/loading.png',
+    preloaderLogo: 'assets/images/logo.png',
+    tile: 'assets/images/tile.png',
+    playAudio: 'assets/images/play_audio.png',
+    background: 'assets/images/background.png',
+    title: 'assets/images/title.png',
+    welldone: 'assets/images/well-done.png',
+    cloud: 'assets/images/cloud.png',
+    rightArrow: 'assets/images/right_arrow.png',
+    leftArrow: 'assets/images/left_arrow.png',
+    mute: 'assets/images/mute.png',
+    unmute: 'assets/images/unmute.png',
+  },
 
-    images:{
-        preloaderLoading:'assets/images/loading.png',
-        preloaderLogo:'assets/images/logo.png',
-        tile:'assets/images/tile.png',
-        playAudio:'assets/images/play_audio.png',
-        background:'assets/images/background.png',
-        title:'assets/images/title.png',
-        welldone:'assets/images/well-done.png'
-    },
+  letters: [
+    'c',
+    'h',
+    'a',
+    'r',
+    'l',
+    'i',
+    'e',
+    'b',
+    'o',
+    'd',
+    'm',
+    'a',
+    'n',
+    'd',
+    'a',
+    'n',
+    'y',
+    'o',
+    'n',
+    'a',
+    'f',
+    'o',
+    'o',
+    'b',
+    'a',
+    'r',
+    'ł',
+    'u',
+    'k’',
+    'é',
+  ],
+  categories: {
+    People: [
+      {
+        word: 'Charlie',
+        characters: ['C', 'h', 'a', 'r', 'l', 'i', 'e'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+      {
+        word: 'Bodman',
+        characters: ['B', 'o', 'd', 'm', 'a', 'n'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+      {
+        word: 'Dan',
+        characters: ['D', 'a', 'n'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+      {
+        word: 'Yona',
+        characters: ['Y', 'o', 'n', 'a'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+      {
+        word: 'Foo',
+        characters: ['F', 'o', 'o'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+      {
+        word: 'Bar',
+        characters: ['B', 'a', 'r'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+    ],
+    Animals: [
+      {
+        word: 'łuk’é',
+        characters: ['ł', 'u', 'k’', 'é'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+    ],
+    Things: [
+      {
+        word: 'łuk’é',
+        characters: ['ł', 'u', 'k’', 'é'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+    ],
+    Something1: [
+      {
+        word: 'łuk’é',
+        characters: ['ł', 'u', 'k’', 'é'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+    ],
+    Something2: [
+      {
+        word: 'łuk’é',
+        characters: ['ł', 'u', 'k’', 'é'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+    ],
+    Something3: [
+      {
+        word: 'łuk’é',
+        characters: ['ł', 'u', 'k’', 'é'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+    ],
+    Something4: [
+      {
+        word: 'łuk’é',
+        characters: ['ł', 'u', 'k’', 'é'],
+        translation: 'translation',
+        audio: 'assets/sounds/sample.mp3',
+        image: 'assets/images/bear.jpg',
+      },
+    ],
+  },
+}
 
-    letters:[],
-
-    words:[
-        {
-            word:"łuk’é",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"chíze",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"asd oaskd o",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"dįnį deneyu",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"dįnį",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"nąntł’er",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"xait’ázį́",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        },  
-        {
-            word:"dekai",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"ek’egwi",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"yaghe",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"eghéasdzé",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"jíedeniyéri",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }, 
-        {
-            word:"bagheasdtél",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        },  
-        {
-            word:"tthábes",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        },  
-        {
-            word:"łus",
-            translation:"translation",
-            audio:'assets/sounds/sample.mp3',
-            image:'assets/images/bear.jpg'
-        }
-    ]
-
-};
-
-
-let gameConfig = {};
+let gameConfig = {}
 
 export default {
+  setConfig: (config) => {
+    gameConfig = Object.assign({}, defaultGameConfig, config, gameConfig)
+  },
 
-    setConfig:(config) => {
-        gameConfig = Object.assign({}, defaultGameConfig, config, gameConfig);
-    },
+  reset: () => {
+    gameConfig = {}
+  },
 
-    reset:() => {
-        gameConfig = {};
-    },
-
-    getConfig:() => {
-        return gameConfig;
-    }
+  getConfig: () => {
+    return gameConfig
+  },
 }
